0570016b1528e6540bc507826f72f0359a85a7d0
Allan Michay <allan.michay@gmail.com>
2025-08-04 12:48:07 +0200

    chore(ui): add missing tailwind settings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,9 +7,14 @@ export default <Partial<Config>>{
         beige: '#F7F3F0',
         card: '#FF7342',
         'main-border': 'rgba(27, 27, 27, 0.1)',
+        black: '#1B1B1B',
+        focus: '#B176FF',
       },
       spacing: {
         tablet: '720px',
+      },
+      fontFamily: {
+        main: ['DM Sans', 'sans-serif'],
       },
     },
   },
